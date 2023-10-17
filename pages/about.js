eea9fb1cdb2a05f81fde2d004b7f2a9715ce4ac1
NicoleAlexBarrero <113063376+NicoleAlexBarrero@users.noreply.github.com>
2023-10-17 18:10:51 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -24,14 +24,10 @@ export default function About(props) {
       <Card>
         <Card.Body>
           <p>
-          I am currently pursuing an Ontario College Diploma in Software Development at Seneca Polytechnic College, where I am gaining a strong foundation in programming languages and relevant coursework such as  
-           Web Programming for Apps and Services, IT Project Management, and Advanced Database Services. My coursework includes proficiency in JavaScript, HTML, CSS, React.js, MongoDB, and Node.js.
-In my current role as an IT Support Technician at TJ Bloc Pilates, I have honed my technical skills by providing prompt and efficient technical support to employees. I have also played a key role in the installation, configuration, and maintenance of computer systems, ensuring seamless operations within the organization. Moreover, I have conducted user training sessions on various technology and software applications, including Adobe Premiere Pro and DaVinci Resolve, to enhance user proficiency.
-Additionally, my experience as a Social Media Manager at TJ Bloc Pilates has equipped me with valuable skills in developing and implementing effective social media strategies. I successfully increased the companies social media following by devising engaging content and managing various social media accounts, such as Instagram. My ability to analyze social media data and user engagement has enabled me to make data-driven decisions for content optimization, ultimately contributing to the companies online presence and brand image.
+          I am currently pursuing an Ontario College Diploma in Software Development at Seneca Polytechnic College.
           </p>
           <p>
-          I am enthusiastic about the opportunity to apply my technical knowledge, problem-solving abilities, and passion for programming to contribute to your software development team. I am committed to continuous learning and growth in the field of programming, and I am confident that my skills and dedication align well with the goals and values of company.
-I look forward to the opportunity to discuss how my background and qualifications can be an asset to your team. You can reach me at nicalexbarrero@gmail.com or (647) 974-1163 to schedule an interview.
+          I am enthusiastic about the opportunity to apply my technical knowledge, problem-solving abilities, and passion for programming to contribute to your software development team. 
           </p>
           <p>
             Its difficult to choose a favourite, but you can search 
